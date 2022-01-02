547cd13bd849dd7ba1d0eed981a546a5de0f6208
kimutaiamos <amos.kimutai@student.moringashool.com>
2022-01-02 10:21:21 +0300

    added model profile

--- neighbours/models.py
+++ neighbours/models.py
@@ -4,3 +4,27 @@ from tinymce.models import HTMLField
 from pyuploadcare.dj.models import ImageField
 # Create your models here.
 
+
+class Profile(models.Model):
+    prof_pic = ImageField(blank=True, manual_crop='800x800')
+    bio = HTMLField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def save_profile(self):
+        self.save()
+    
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
