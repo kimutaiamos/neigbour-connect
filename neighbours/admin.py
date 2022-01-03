47d0517e382125d44223714444f2e9fd1ddf75a7
kimutaiamos <amos.kimutai@student.moringashool.com>
2022-01-03 12:30:26 +0300

    added comments

--- neighbours/admin.py
+++ neighbours/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post,Comments,Profile
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(Profile)
