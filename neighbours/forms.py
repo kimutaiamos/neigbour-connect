9e87fabb5a2728f6f2f6d294daf045fec9516d2a
kimutaiamos <amos.kimutai@student.moringashool.com>
2022-01-05 17:02:23 +0300

    added a profile form

--- neighbours/forms.py
+++ neighbours/forms.py
@@ -16,3 +16,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['likes', 'post_date', 'profile']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
