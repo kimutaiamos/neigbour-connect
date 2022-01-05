e0e646c1766ce51afd992e6660ab250459c3f201
kimutaiamos <amos.kimutai@student.moringashool.com>
2022-01-05 17:54:15 +0300

    added a js function to display signup fade in and fade out

---
+++ static/js/registration_form.js
@@ -0,0 +1,14 @@
+$("#signup").click(function() {
+    $("#first").fadeOut("fast", function() {
+    $("#second").fadeIn("fast");
+    });
+    });
+    
+    $("#signin").click(function() {
+    $("#second").fadeOut("fast", function() {
+    $("#first").fadeIn("fast");
+    });
+    });
+    
+    
+      
